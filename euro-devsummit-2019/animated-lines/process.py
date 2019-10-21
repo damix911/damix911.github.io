89868b8be1d42e2c7354883ca48ab1121645ee12
Dario D'Amico <DD'Amico@esri.com>
2019-10-21 11:49:53 -0700

    wip

--- euro-devsummit-2019/animated-lines/process.py
+++ euro-devsummit-2019/animated-lines/process.py
@@ -5,7 +5,7 @@ with open("lines-original.json", "r") as f:
   lines = json.load(f)
 
 for line in lines:
-  line["color"] = random.choice([[161, 210, 135], [109, 155, 195]])
+  line["color"] = random.choice([[217, 134, 149], [109, 155, 195]])
 
 with open("lines.json", "w") as f:
   json.dump(lines, f)
