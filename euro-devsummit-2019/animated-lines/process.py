182d3ef474c4ae33366c8095aed36e598321fdb0
Dario D'Amico <DD'Amico@esri.com>
2019-10-21 11:52:42 -0700

    update colors

--- euro-devsummit-2019/animated-lines/process.py
+++ euro-devsummit-2019/animated-lines/process.py
@@ -4,8 +4,12 @@ import random
 with open("lines-original.json", "r") as f:
   lines = json.load(f)
 
+surf_green = [161, 210, 135]
+shimmering_blush = [217, 134, 149]
+cerulean_frost = [109, 155, 195]
+
 for line in lines:
-  line["color"] = random.choice([[217, 134, 149], [109, 155, 195]])
+  line["color"] = random.choice([cerulean_frost, shimmering_blush])
 
 with open("lines.json", "w") as f:
   json.dump(lines, f)
