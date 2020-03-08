357bd1366881ce34f736d97f5fd6a21a5d956855
Dario D'Amico <dario@localhost.localdomain>
2020-03-08 01:23:03 -0800

    Updated deck.gl@next

--- deckglbuilds/next/deploy.sh
+++ deckglbuilds/next/deploy.sh
@@ -4,25 +4,25 @@
 
 TARGET_DIR=/home/dario/Programming/esri/damix911.github.io/deckglbuilds/next
 
-# cd /home/dario/Programming/esri/uber/deck.gl
-# yarn build
-# mkdir -p $TARGET_DIR/deck.gl@next
-# mv modules/main/dist.min.js $TARGET_DIR/deck.gl@next/dist.min.js
+cd /home/dario/Programming/esri/uber/deck.gl
+yarn build
+mkdir -p $TARGET_DIR/deck.gl@next
+mv modules/main/dist.min.js $TARGET_DIR/deck.gl@next/dist.min.js
 
-# cd /home/dario/Programming/esri/uber/deck.gl/modules/layers
-# npm run build-bundle
-# mkdir -p $TARGET_DIR/@deck.gl/layers@next
-# mv dist.min.js $TARGET_DIR/@deck.gl/layers@next/dist.min.js
+cd /home/dario/Programming/esri/uber/deck.gl/modules/layers
+npm run build-bundle
+mkdir -p $TARGET_DIR/@deck.gl/layers@next
+mv dist.min.js $TARGET_DIR/@deck.gl/layers@next/dist.min.js
 
-# cd /home/dario/Programming/esri/uber/deck.gl/modules/geo-layers
-# npm run build-bundle
-# mkdir -p $TARGET_DIR/@deck.gl/geo-layers@next
-# mv dist.min.js $TARGET_DIR/@deck.gl/geo-layers@next/dist.min.js
+cd /home/dario/Programming/esri/uber/deck.gl/modules/geo-layers
+npm run build-bundle
+mkdir -p $TARGET_DIR/@deck.gl/geo-layers@next
+mv dist.min.js $TARGET_DIR/@deck.gl/geo-layers@next/dist.min.js
 
-# cd /home/dario/Programming/esri/uber/deck.gl/modules/arcgis
-# npm run build-bundle
-# mkdir -p $TARGET_DIR/@deck.gl/arcgis@next
-# mv dist.min.js $TARGET_DIR/@deck.gl/arcgis@next/dist.min.js
+cd /home/dario/Programming/esri/uber/deck.gl/modules/arcgis
+npm run build-bundle
+mkdir -p $TARGET_DIR/@deck.gl/arcgis@next
+mv dist.min.js $TARGET_DIR/@deck.gl/arcgis@next/dist.min.js
 
 cd $TARGET_DIR
 git add .
